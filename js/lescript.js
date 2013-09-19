8dcf42c846ac7bddd4b2e3cb75dd26ddfc94fd12
Zach <zmcartor@gmail.com>
2013-09-18 20:53:34 -0400

    Final decision content and logic

--- js/lescript.js
+++ js/lescript.js
@@ -5,25 +5,20 @@ $(function(){
       responsiveBucket = 0;
 
   var computeResult = function(){
-    if (nativeBucket > responsiveBucket) {
-      console.log("Native!!");
-      console.log(nativeBucket);
-      return;
-    }
-    console.log(responsiveBucket);
-    console.log("responsive");
+    debugger;
+    var finalContent;
+    if (nativeBucket > responsiveBucket) finalContent = $('#finalNative').html();
+    else if (nativeBucket < responsiveBucket) finalContent = $('#finalResponsive').html();
+    $('main').hide().empty().html(finalContent).fadeIn();
   };
 
 $('main').delegate('button','click',function(e){
   currentQuestion+=1;
-  if (currentQuestion === questionCount){
-    computeResult();
-  }
   var bucket = $(e.target).data('bucket');
   if(bucket === 'r') responsiveBucket+=1;
   else if(bucket ==='n') nativeBucket+=1;
-
-  $('main').hide().empty().html($('#q'+currentQuestion+'Content').html()).fadeIn();
+  if (currentQuestion === questionCount) computeResult();
+  else $('main').hide().empty().html($('#q'+currentQuestion+'Content').html()).fadeIn();
   $('li.activeQuestion').removeClass('activeQuestion');
   $('li#q'+currentQuestion).addClass('activeQuestion');
 });
